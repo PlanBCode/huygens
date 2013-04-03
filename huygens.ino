f70dfb9ea7ed6f48ca7ec067b1fe5f2adb2bd982
peteruithoven <peter@peteruithoven.nl>
2013-04-03 23:33:23 +0200

    filled in pins

--- huygens.ino
+++ huygens.ino
@@ -6,41 +6,49 @@
 
 int numRoutes = 4;
 int currentRoute = 0;
-float pointerPosition = 0; //percentage 0 till 1
-int state = DEFAULT_STATE;
-int motorSpeed = 0;
-boolean motorDirFoward = true;
-
-// pedal pins
-int pedalInputTopPin
-int pedalInputBottomPin
-// route pins
-int routeInputPins[] = {}; // input of route selection slider
-int routeChoiceLedsPins[] = {} // led pins lightning route choice in closet
-int routeLedsPins[] = { // led pins along routes to light objects
+int routeTracks[] = { // tracks per object along routes
   {},
   {},
   {},
   {},
   {}
 } 
+float pointerEndPosition = 0; //percentage 0 till 1
+int state                = DEFAULT_STATE;
+int motorSpeed           = 0;
+boolean motorDirFoward   = true;
+
 // motor pins
-int motorPosInputPin // motor position potmeter
-int motorSpeedPin   =  // PWM / D2
-int motorEnablePin  = 
-int motorInversePin = 
-int motorSFPin      = // status flag
-int motorFBPin      = // analog power consumption feedback
+int motorPosInputPin  = A0; // motor position potmeter
+int motorSpeedPin     = 12; // PWM / D2
+int motorSFPin        = 11; // status flag
+//int motorFBPin      = A0; // analog power consumption feedback
+int motorEnablePin    = 10;
+int motorSlewPin      = 9;
+int motorInversePin   = 8;
+// route pins
+int routeInputPins[]      = {7,6,5,4}; // input of route selection slider
+int routeChoiceLedsPins[] = {} // led pins lightning route choice in closet (pins 30 - 53)
+int routeLedsPins[]       = { // led pins along routes to light objects (pins 30 - 53)
+  {},
+  {},
+  {},
+  {},
+  {}
+} 
+// pedal pins
+int pedalInputTopPin    = 3
+int pedalInputBottomPin = 2
 // audio pins
-int audioEnTXPin    = 
-int audioEnRXPin    =
-int audioNlTXPin    = 
-int audioNlRXPin    = 
+//int audioTXPin      = 18;
+//int audioRXPin      = 19;
 
 void setup() {
   // set pin modes
   
   //enable pull-up resistors
+  
+  digitalWrite(motorSlewPin,LOW);
   
   Serial.begin(9600);
 }
